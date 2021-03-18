1f665759c22b2a35427f4dd1d3cfa45159175329
Diogo Dias <DiogoDias@criticalmanufacturing.com>
2021-03-18 16:29:07 +0000

    Create the Comments micro-service

---
+++ blog/comments/index.js
@@ -0,0 +1,45 @@
+// Libraries
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+
+
+// Constants
+const port = 4001;
+const commentsByPostId = {};
+
+const app = express();
+
+// Config
+app.use(bodyParser.json());
+
+// Routes
+app.get('/posts/:id/comments', (req, res) => {
+    const { postId } = req.params.id;
+
+    res.send(commentsByPostId[postId] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+    const { content } = req.body;
+    const { postId } = req.params.id;
+
+    const commentId = randomBytes(4).toString('hex');
+
+    const comments = commentsByPostId[postId] || [];
+
+    comments.push({
+        id: commentId,
+        content
+    });
+
+    commentsByPostId[postId] = comments;
+
+    res.status(201).send(comments);
+});
+
+
+app.listen(port, () => {
+    console.log(`Comments service listening in port: ${port}`);
+});
