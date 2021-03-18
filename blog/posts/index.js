0e0ce58d36257fbb60580b91460d69f397a481eb
Diogo Dias <DiogoDias@criticalmanufacturing.com>
2021-03-18 16:16:10 +0000

    Create Post service

---
+++ blog/posts/index.js
@@ -0,0 +1,40 @@
+// Libraries
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+
+// Constants
+const port = 4000;
+
+const posts = {};
+
+
+const app = express();
+
+// Configs
+app.use(bodyParser.json());
+
+
+// Routes
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+    const { title } = req.body;
+
+    const id = randomBytes(4).toString('hex');
+
+    posts[id] = {
+        id,
+        title
+    };
+
+    res.status(201).send(posts[id]);
+});
+
+
+app.listen(port, () => {
+    console.log(`Post service listening in port: ${port}`);
+});
