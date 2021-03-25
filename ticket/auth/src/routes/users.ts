2a146e1966131654832299ac6450dd4ee1d82dd1
Diogo Dias <DiogoDias@criticalmanufacturing.com>
2021-03-25 11:45:36 +0000

    Create a validation requets middleware

--- ticket/auth/src/routes/users.ts
+++ ticket/auth/src/routes/users.ts
@@ -1,9 +1,9 @@
 import { Router, Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
 
 import { BadRequestError } from '../errors/bad-request-error';
-import { RequestValidationError } from '../errors/request-validation-error';
+import { validateRequest } from '../middlewares/validate-request';
 import User from '../models/user';
 
 const router = Router();
@@ -25,13 +25,8 @@ router.post('/signup',
             .isLength({ min: 4, max: 20 })
             .withMessage('Password must be between 4 and 20 characters')
     ],
+    validateRequest,
     async (req: Request, res: Response) => {
-        const errors = validationResult(req);
-
-        if (!errors.isEmpty()) {
-            throw new RequestValidationError(errors.array());
-        }
-
         const { email, password } = req.body;
 
         const existingUser = await User.findOne({ email });
@@ -62,11 +57,24 @@ router.post('/signup',
         };
 
         res.status(201).send(user);
-    });
+    }
+);
 
-router.post('/signin', (req, res) => {
-
-});
+router.post('/signin',
+    [
+        body('email')
+            .isEmail()
+            .withMessage('Email must be valid'),
+        body('password')
+            .trim()
+            .notEmpty()
+            .withMessage('You must supply a password')
+    ],
+    validateRequest,
+    async (req: Request, res: Response) => {
+        const { email, password } = req.body;
+    }
+);
 
 router.post('/signout', (req, res) => {
 
