08cecf001b21d252f49f79612b4b8479ff0356c0
Diogo Dias <DiogoDias@criticalmanufacturing.com>
2021-03-26 09:24:46 +0000

    Add tests to signout

--- ticket/auth/src/routes/__test__/users.test.ts
+++ ticket/auth/src/routes/__test__/users.test.ts
@@ -69,5 +69,78 @@ describe('Signup tests', () => {
             .expect(201);
 
         expect(response.get('Set-Cookie')).toBeDefined();
+        expect(response.get('Set-Cookie')[0]).not.toContain('express:sess=;');
+    });
+});
+
+describe('Signin tests', () => {
+    it('should fail when a email that doesn\'t exists is supplied', async () => {
+        return request(app)
+            .post(`${baseURI}/signin`)
+            .send({
+                email,
+                password
+            })
+            .expect(400);
+    });
+
+    it('should fail when an incorrect password is supplied', async () => {
+        await request(app)
+            .post(`${baseURI}/signup`)
+            .send({
+                email,
+                password
+            })
+            .expect(201);
+
+        await request(app)
+            .post(`${baseURI}/signin`)
+            .send({
+                email,
+                password: 'wrong password'
+            })
+            .expect(400);
+    });
+
+    it('should signin successfully and return a correct cookie', async () => {
+        await request(app)
+            .post(`${baseURI}/signup`)
+            .send({
+                email,
+                password
+            })
+            .expect(201);
+
+        const response = await request(app)
+            .post(`${baseURI}/signin`)
+            .send({
+                email,
+                password
+            })
+            .expect(200);
+
+        expect(response.get('Set-Cookie')).toBeDefined();
+        expect(response.get('Set-Cookie')[0]).not.toContain('express:sess=;');
+    });
+});
+
+describe('Signout tests', () => {
+    it('should clears the cookie when signing out', async () => {
+        // When singing up the user is also signed in
+        await request(app)
+            .post(`${baseURI}/signup`)
+            .send({
+                email,
+                password
+            })
+            .expect(201);
+
+        const response = await request(app)
+            .post(`${baseURI}/signout`)
+            .send()
+            .expect(200);
+
+        expect(response.get('Set-Cookie')).toBeDefined();
+        expect(response.get('Set-Cookie')[0]).toContain('express:sess=;');
     });
 });
