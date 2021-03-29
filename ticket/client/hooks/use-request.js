f4845cc0c5818ccc4375f8bfe7e2c3b596439c73
Diogo Dias <DiogoDias@criticalmanufacturing.com>
2021-03-29 14:52:52 +0100

    Add onSuccess callback to hook and fix error

--- ticket/client/hooks/use-request.js
+++ ticket/client/hooks/use-request.js
@@ -2,24 +2,27 @@ import { useState } from 'react';
 import axios from 'axios';
 
 
-export default ({ url, method, body }) => {
+export default ({ url, method, body, onSuccess }) => {
     const [errors, setErrors] = useState(null);
 
     const doRequest = async () => {
         setErrors(null);
-        
+
         try {
             const response = await axios[method](
                 url,
                 body
             );
+
+            onSuccess(response.data);
+
             return response.data;
         } catch (err) {
             setErrors(
                 <div className="alert alert-danger">
                     <h4>Ooops....</h4>
                     <ul className="my-0">
-                        {errors.map(error => (
+                        {err.map(error => (
                             <li key={error.message}>
                                 {error.message}
                             </li>
--- ticket/client/pages/auth/signup.js
+++ ticket/client/pages/auth/signup.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import axios from 'axios';
+import { Router } from 'next/router';
 
 // Hooks
 import useRequest from '../../hooks/use-request';
@@ -14,7 +14,8 @@ export default () => {
         body: {
             email,
             password
-        }
+        },
+        onSuccess: () => Router.push('/')
     });
 
     const onSubmit = async (e) => {
